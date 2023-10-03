e06dcfce4364bcbad4cb62671b5704cb81b18401
Vincetroid <vince_trance@hotmail.com>
2023-10-03 00:27:33 -0600

    Sort import via eslint-plugin-simple-import-sort and add conf to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,55 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+        'prettier/prettier': [
+          'error',
+          {
+            endOfLine: 'auto',
+          },
+        ],
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              [
+                '^react$',
+                '^react-native$',
+                '^[a-z]',
+                '^@react',
+                '^@',
+                '^@/redux-content',
+                '^@/components',
+                '^@/hooks',
+                '^@/utils',
+                '^@/assets',
+                '^@/translations',
+                '^@global-styles',
+                '^@/theme',
+                '^\\.\\.(?!/?$)',
+                '^\\.\\./?$',
+                '^\\./(?=.*/)(?!/?$)',
+                '^\\.(?!/?$)',
+                '^\\./?$',
+                '^\\u0000',
+              ],
+            ],
+          },
+        ],
+      },
+    },
+  ],
+  ignorePatterns: ['src/Api/*'],
 };
