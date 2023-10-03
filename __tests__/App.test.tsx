1e996962ff393aaea9687494fe849e3a0befbe83
Vincetroid <vince_trance@hotmail.com>
2023-10-02 23:38:39 -0600

    Add TODO about missing Jest configuration
    Remove tests from pre-commit

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -8,6 +8,9 @@ import { it } from '@jest/globals';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+//TODO: Solve
+// Invariant Violation: TurboModuleRegistry.getEnforcing(...): 'PlatformConstants' could not be found. Verify that a module by this name is registered in the native binary.
+// Some guidance but to start: https://react-native-async-storage.github.io/async-storage/docs/help/troubleshooting/#ios-cocoapods-issues
 it('renders correctly', () => {
   renderer.create(<App />);
 });
