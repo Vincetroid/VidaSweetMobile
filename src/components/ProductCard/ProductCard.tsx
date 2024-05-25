5733b9c0fe1b60db60cd3e1f737e3bf6764c3813
Vincetroid <vince_trance@hotmail.com>
2024-05-25 02:45:53 -0600

    Complete useFetchProductImages

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Dimensions, Image, Text, TouchableOpacity, View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 import Toast from 'react-native-simple-toast';
-import { getProductImage } from '@/fb/storageQueries';
 import { Colors } from '@/global-styles';
 import { ProductItem } from '@/interfaces';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { addProduct } from '@/redux-content';
 import { useAppDispatch } from '@/hooks';
+import { useFetchProductImages } from '@/hooks/useFetchProductImages';
 import { formatCurrency } from '@/utils';
 import { AddRemoveToFavorites } from '../AddRemoveToFavorites';
 import { styles } from './ProductCard.styles';
@@ -16,7 +16,7 @@ export const ProductCard = ({ product }: { product: ProductItem }) => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const { img, title, price, isFavorite = false } = product;
-  const [currentImage, setCurrentImage] = useState<string>('');
+  const { currentImage } = useFetchProductImages(img);
 
   const cardGap = 30;
   const cardWidth = (Dimensions.get('window').width - cardGap * 3) / 2;
@@ -36,16 +36,6 @@ export const ProductCard = ({ product }: { product: ProductItem }) => {
       },
     );
   };
-
-  // ESTA LOGICA VA AL HOOK useFetchProductImages
-  const init = async () => {
-    const productImage = await getProductImage(img.toString());
-    setCurrentImage(productImage);
-  };
-
-  useEffect(() => {
-    init();
-  }, []);
 
   return (
     <View
--- src/hooks/useFetchProductImages.ts
+++ src/hooks/useFetchProductImages.ts
@@ -1,36 +1,25 @@
 import { useEffect, useState } from 'react';
-import { useNavigation } from '@react-navigation/native';
 import { getProductImage } from '@/fb/storageQueries';
 import handleErrors from '@/utils/handleErrors';
 
 export const useFetchProductImages = (img: number) => {
-  const navigation = useNavigation();
   const [currentImage, setCurrentImage] = useState<string>('');
   const [loader, setLoader] = useState<boolean>(false);
 
-  //TE QUEDASTE AQUI
-  const algo = async (image: number) => {
+  const init = async (image: number) => {
     const productImage = await getProductImage(image.toString());
-    console.log('productImage');
-    console.log(productImage);
     setCurrentImage(productImage);
   };
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', async () => {
-      setLoader(true);
-      try {
-        console.log('img:');
-        console.log(img);
-        algo(img);
-        setLoader(false);
-      } catch (error) {
-        handleErrors(error.code);
-      }
-    });
-
-    return unsubscribe;
-  }, [img, navigation]);
+    setLoader(true);
+    try {
+      init(img);
+      setLoader(false);
+    } catch (error) {
+      handleErrors(error.code);
+    }
+  }, []);
 
   return {
     currentImage,
