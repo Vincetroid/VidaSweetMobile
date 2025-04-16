62acc94c7d9ded214ca9267f5cd55af1b55c3250
Vincetroid <vince_trance@hotmail.com>
2025-04-16 11:59:28 -0600

    Add @react-native-firebase/messaging and implement basic push notifications getting FCM token manually and put it in firebase console

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { LogBox } from 'react-native';
+import { PermissionsAndroid } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import messaging from '@react-native-firebase/messaging';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { themeStyles } from '@/global-styles';
 import addFontAwesomeIcons from './src/assets/icons/FontAwesomeIconsHandler';
@@ -24,7 +26,30 @@ const App = () => {
     }
   }
 
+  async function requestUserPermission() {
+    PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+    );
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    console.log('Enableddd: ', enabled);
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+  }
+
+  const getMessagingToken = async () => {
+    const messagingToken = await messaging().getToken();
+    console.log('messagingToken');
+    console.log(messagingToken);
+    return messagingToken;
+  };
+
   useEffect(() => {
+    requestUserPermission();
+    getMessagingToken();
     addFontAwesomeIcons();
 
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
@@ -47,10 +72,7 @@ const App = () => {
 
   const ReactNavigationThemeJustDefaultBgColor = {
     ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      background: themeStyles.background,
-    },
+    colors: { ...DefaultTheme.colors, background: themeStyles.background },
   };
 
   return (
